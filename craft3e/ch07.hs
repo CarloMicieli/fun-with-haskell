e2415c40de829e2c58bc0205693d42419b88beed
CarloMicieli <carlo.micieli@gmail.com>
2014-11-23 10:37:38 +0100

    Updated

--- craft3e/ch07.hs
+++ craft3e/ch07.hs
@@ -132,8 +132,8 @@ reverse' (x:xs) = reverse' xs ++ [x]
 
 unzip' :: [(a, b)] -> ([a], [b])
 unzip' []         = ([], [])
-unzip' ((x,y):ys) = let pair = unzip' ys
-                     in (x:fst pair, y:snd pair)
+unzip' ((x,y):ys) = let (f, s) = unzip' ys
+                     in (x : f, y : s)
 
 {- Ex 7.12 -}
 minAndMax :: [Integer] -> (Integer, Integer)
@@ -147,3 +147,85 @@ minAndMax' []   = error "Prelude.minAndMax: empty list"
 minAndMax' [x]  = (x, x)
 minAndMax' xs   = let (y:ys) = iSort xs
                    in (y, last ys)
+
+{- Ex 7.16 -}
+iSort' :: (Integer -> [Integer] -> [Integer]) -> [Integer] -> [Integer]
+iSort' _ []     = []
+iSort' f (x:xs) = f x (iSort' f xs)
+
+ins' :: Integer -> [Integer] -> [Integer]
+ins' x []        = [x]
+ins' x (y:ys)    | x >= y     = x : (y : ys)
+                 | otherwise  = y : (ins' x ys)
+
+ins'' :: Integer -> [Integer] -> [Integer]
+ins'' x []       = [x]
+ins'' x l@(y:ys) | x == y    = l
+                 | x <= y    = x : l
+                 | otherwise = y : (ins'' x ys)
+
+{- Ex 7.19 -}
+sortPairs :: [(Integer, Integer)] -> [(Integer, Integer)]
+sortPairs []     = []
+sortPairs (x:xs) = ins x (sortPairs xs)
+  where ins x       []     = [x]
+        ins x@(k,v) (y@(k',_):ys) | k <= k'   = x : y : ys
+                                  | otherwise = y : (ins x ys)
+
+partition :: (a -> Bool) -> [a] -> ([a], [a])
+partition _ []     = ([], [])
+partition p (x:xs) = let (t, f) = partition p xs
+                      in if (p x) then (x : t, f)
+                         else          (t, x : f)
+
+qSort :: [Integer] -> [Integer]
+qSort []         = []
+qSort (pivot:xs) = let (left, right) = partition (<=pivot) xs
+                    in (qSort left) ++ [pivot] ++ (qSort right)
+
+{- Ex 7.20 -}
+drop' :: Int -> [a] -> [a]
+drop' _ []     = []
+drop' n l@(x:xs) | n <= 0    = l
+                 | n > 0     = drop' (n - 1) xs
+
+drop_prop :: Int -> [Integer] -> Bool
+drop_prop n xs = drop n xs == drop' n xs
+
+splitAt' :: Int -> [a] -> ([a], [a])
+splitAt' _ []       = ([], [])
+splitAt' n l@(x:xs) | n <= 0    = ([], l)
+                    | otherwise = let (f, s) = splitAt' (n - 1) xs
+                                   in (x : f, s)
+
+splitAt_prop :: Int -> [Integer] -> Bool
+splitAt_prop n xs = splitAt n xs == splitAt' n xs
+
+{- Ex 7.21 -}
+take' :: Int -> [a] -> [a]
+take' n (x:xs) | n > 0 = x : take' (n - 1) xs
+take' _ _      = []
+
+{- Ex 7.22 -}
+zip' :: ([a], [b]) -> [(a, b)]
+zip' ((x:xs),(y:ys)) = (x,y) : zip' (xs, ys)
+zip' _               = []
+
+zipAndUnzip_prop :: [Integer] -> [Integer] -> Property
+zipAndUnzip_prop xs ys = (length xs == length ys) ==>
+                         let ps = (xs, ys)
+                          in unzip' (zip' ps) == ps
+
+{- Ex 7.23 -}
+zip3' :: [a] -> [b] -> [c] -> [(a, b, c)]
+zip3' (x:xs) (y:ys) (z:zs) = (x, y, z) : (zip3' xs ys zs)
+zip3' _      _      _      = []
+
+zip3'' :: [a] -> [b] -> [c] -> [(a, b, c)]
+zip3'' xs ys zs = map (\ ((x,y),z) -> (x,y,z)) $ zip (zip xs ys) zs
+
+{- Ex 7.24 -}
+qSort' :: [Integer] -> [Integer]
+qSort' []         = []
+qSort' (pivot:xs) = let (l, r) = partition (<= pivot) xs
+                     in (qSort' r) ++ [pivot] ++ (qSort' l)
